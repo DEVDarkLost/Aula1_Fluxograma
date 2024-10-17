4d528e9aadc02daa897f7b35756cfd06c5f25245
Nicolas <nicolas.seabra09@gmail.com>
2024-10-17 17:59:43 -0300

    Add files via upload

---
+++ Aula1.py
@@ -0,0 +1,27 @@
+list
+#vamos fazer atribuições para as variaveis
+print("Para começar vamos brincar com numeors")
+a = 10
+b = 20
+c = 0
+
+#dado do tipo string(texto)
+texto = 'isso é um texto'
+print(texto)
+
+# dado do tipo float(real)
+real = 10.5
+print(real)
+
+# dado do tipo bool(logico)
+boleano = True
+print(boleano)
+
+#Estamos perguntando se A é maior que B se não faça isso
+if a > b: (
+    print("a é maior que b")
+    )
+else:
+    print("a é menor que b")
+
+
